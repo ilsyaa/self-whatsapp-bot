dfa23963cb909b5090baa076cdb85778dbcd341f
ilsyaa <ilham.shindou@gmail.com>
2024-10-13 18:04:13 +0700

    refactor: menambah kemungkinan pesan di fitur block unblock

--- src/command/owner/block.js
+++ src/command/owner/block.js
@@ -4,12 +4,14 @@ module.exports = {
     cmd: ['block'],
     run: async ({ m, sock }) => {
         if(!m.senderIsOwner) return
+        if(m.mentionedJid && m.mentionedJid.length == 0) return m._reply("@mentions orang yang ingin diblokir.")
+
         try {
             if(m.quoted) await sock.updateBlockStatus(m.quoted.sender, "block")
             for (let i of m?.mentionedJid || []) {
                 await sock.updateBlockStatus(i, "block")
             }
-            m._reply("badut berhasil diblokir.")
+            m._reply(`${m.mentionedJid.length} badut berhasil diblokir.`)
         } catch(error) {
             await m._reply(error.message)
         }        
--- src/command/owner/unblock.js
+++ src/command/owner/unblock.js
@@ -4,12 +4,14 @@ module.exports = {
     cmd: ['unblock'],
     run: async ({ m, sock }) => {
         if(!m.senderIsOwner) return
+        if(m.mentionedJid && m.mentionedJid.length == 0) return m._reply("@mentions orang yang ingin di unblokir.")
+
         try {
             if(m.quoted) await sock.updateBlockStatus(m.quoted.sender, "unblock")
             for (let i of m?.mentionedJid || []) {
                 await sock.updateBlockStatus(i, "unblock")
             }
-            m._reply("badut berhasil diunblokir.")
+            m._reply(`${m.mentionedJid.length} badut berhasil diunblokir.`)
         } catch(error) {
             await m._reply(error.message)
         }        
